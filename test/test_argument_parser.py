ce2138b771fdf835cc4851864abd557eadbb1e27
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-05-27 22:12:45 +0100

    test the long form of the parameters

--- test/test_argument_parser.py
+++ test/test_argument_parser.py
@@ -4,7 +4,7 @@ from elementary_cellular_automaton import _make_parser
 
 
 class TestMakeParser(unittest.TestCase):
-    def test(self):
+    def test_short_options(self):
         parser = _make_parser()
         settings = parser.parse_args(
             [
@@ -15,3 +15,15 @@ class TestMakeParser(unittest.TestCase):
         self.assertEqual(settings.generations, 6)
         self.assertEqual(settings.rule, 30)
         self.assertEqual(settings.output, 'a.svg')
+
+    def test_long_options(self):
+        parser = _make_parser()
+        settings = parser.parse_args(
+            [
+                '--generations', '10',
+                '--rule', '35',
+                '--output', 'b.svg'
+            ])
+        self.assertEqual(settings.generations, 10)
+        self.assertEqual(settings.rule, 35)
+        self.assertEqual(settings.output, 'b.svg')
